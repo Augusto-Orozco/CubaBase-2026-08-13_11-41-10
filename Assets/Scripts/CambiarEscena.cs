a13c573f56076d9c0c716f7dad39c3725f2bbfd9
Daniel Díaz Romero <A01801486@tec.mx>
2026-08-17 11:47:10 -0600

    Script funcional cambio de escena
    Se escribe en la pared la escena que se desea cambiar

--- Assets/Scripts/CambiarEscena.cs
+++ Assets/Scripts/CambiarEscena.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class CambiarEscena : MonoBehaviour
 {
-    public string nombreEscena;
+    [SerializeField] private string nombreEscena;
+
+    private bool cambiandoEscena = false;
 
     private void OnTriggerEnter(Collider other)
     {
+        // Solo el jugador puede activar el cambio
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (cambiandoEscena)
+            return;
+
+        cambiandoEscena = true;
+
         SceneManager.LoadScene(nombreEscena);
     }
 }
